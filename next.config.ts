9e981d6aec1dd07e0989877e49c7e4631e6ba4c9
AJ Paul <aj.paul@cenitex.vic.gov.au>
2026-04-12 08:05:27 +1000

    added export back

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+output: "export",
   images: {
     unoptimized: true,
     remotePatterns: [new URL("https://github.com/ajaypaulfrancis/cloud_portfolio/**")],
